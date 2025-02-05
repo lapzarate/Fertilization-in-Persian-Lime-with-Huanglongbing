04499652e263cf202a7974b0929d3ed40de440a8
Pérez-Zarate L.A. <132598154+lapzarate@users.noreply.github.com>
2025-02-04 19:22:20 -0600

    Add files via upload

---
+++ Salicylic acid/Salicylic acid .R
@@ -0,0 +1,120 @@
+#plot of the effect of CLas infection and fertilization treatments on the content of Salicylic acid
+
+#Gráfica
+library(ggplot2)
+Plot_SA$Sanidad <- factor(Plot_SA$Sanidad, levels = c("HLB+", "HLB-")) #Sort health status data
+SA1 <- ggplot(Plot_SA, aes(Factor, Prom,fill=Sanidad)) + geom_bar(stat="identity", position=position_dodge(width=0.9),colour = 'black',linewidth=1)+scale_fill_manual(values = c("black","grey"))+geom_errorbar(aes(ymin =Prom, ymax = Prom+DE), width=0.4,linewidth=1, position=position_dodge(width=0.9))+ylab(expression("Salicylic acid (ng g"^-1* " FW)")) + 
+  xlab("Macronutrient levels")+ theme_bw()+theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),axis.text.x = element_text(size=18,vjust=.7),axis.title.y = element_text(size=20),legend.title = element_text(size=30),axis.text = element_text(size = 20),strip.text.x = element_text(size = 30))+facet_grid(. ~ Nivel)
+SA1
+#Adjust Y axis
+
+SA1+coord_cartesian(ylim = c(200, 1700))
+#save
+
+ggsave(filename="SA1.jpg",width=30, height=15, units="cm")
+
+#Packages
+library(openxlsx)
+library(MASS)
+library(rstatix)
+library(ggplot2)
+library(ggpubr)
+library(lme4)
+library(emmeans)
+library(multcomp)
+library(carData)
+library(car)
+library(nlme)
+library(lsmeans)
+
+#File location
+setwd("C:/Users/Luis Alfredo/Documents/MEGAsync/Respaldo/Doctorado/TESIS/Capítulo II_Fertilización/Nuevo análisis/Fitohormonas/SA")
+
+#Load file
+
+SA_final <- within(read.xlsx(xlsxFile="SA_ANOVA_Tukey.xlsx"),{
+  ID = as.factor(ID)
+  Sanidad= as.factor(Sanidad)
+  Factor = as.factor(Factor)
+  Nivel = as.factor(Nivel)
+  Rep = as.factor(Rep)
+  SA= as.numeric(SA)
+})
+
+#Check file
+str(SA_final)
+
+#normality test
+shapiro.test(SA_final$SA)
+#Check outliers
+boxplot(SA_final$SA, col="skyblue", frame.plot=F)
+
+#Box-Cox transformation
+boxcox = boxcox(SA~ Sanidad*Factor*Nivel, data=SA_final,lambda = seq(-2, 2, length = 100))
+#log is the best transformation
+SAA1= lme(log(SA)~ Sanidad*Factor*Nivel, random = ~ 1 | Rep / ID, data=SA_final)
+
+#ANOVA
+joint_tests(SAA1)
+
+#Normalidad
+r = residuals(SAA1,type="normalized", level=0)
+hist(r,freq=F)
+xfit<-seq(min(r),max(r),length=40)
+yfit<-dnorm(xfit, mean=mean(r), sd=sd(r))
+lines(xfit, yfit,col="red",lwd=2)
+shapiro.test(r)
+e1071::kurtosis(r) 
+qqPlot(r)
+#Homogeneidad de varianza
+plot(fitted(SAA1, level=0), r, pch=16, ylab="Normalized residuals", xlab="Predicted values")
+
+abline(h=0, lty=2)
+
+#Tukey
+
+a1 = emmeans(SAA1, ~ Sanidad*Factor*Nivel, type="response"); a1
+SAF <- cld(a1,Letters=letters)
+tidy(SAF)
+plot(SAF)
+
+
+#Second-degree polynomial regression analysis
+
+#Polynomial regression analysis ----------------
+
+#HLB infected plants
+#Macronutrients
+#Fit the data to a polynomial model.
+fitQ = lm(SA~poly(Factor,2,raw=TRUE), data=Poly_SA_macros_sick) 
+#ANOVA 
+anova(fitQ)
+summary(fitQ) #R2, R2adj and p
+
+#micronutrients
+fitQ2 = lm(SA~poly(Factor,2,raw=TRUE), data=Poly_SA_micros_sick) 
+anova(fitQ2)
+summary(fitQ2)
+
+#Healthy plants 
+#Macronutrients
+fitQ3 = lm(SA~poly(Factor,2,raw=TRUE), data=Poly_SA_macros_healthy) 
+anova(fitQ3)
+summary(fitQ3)
+#Micronutrients
+fitQ4 = lm(SA~poly(Factor,2,raw=TRUE), data=Poly_SA_micros_Healthy) 
+anova(fitQ4)
+summary(fitQ4)
+
+#Plot polynomial regression of macronutrients sick vs healthy plants
+
+Plot_SA_macros_poly$Sanidad <- factor(Plot_SA_macros_poly$Sanidad, levels = c("HLB+", "HLB-")) #Sort health status data
+poltcaMa <-  ggplot(data = Plot_SA_macros_poly, aes(x = Factor, y = SA, fill=Sanidad)) + geom_point(aes(shape=Sanidad,stroke = 2),size=3)+geom_smooth(method = "lm", formula = y ~ poly(x,2),color="black")+ scale_fill_manual(values = c("black","gray80")) + scale_x_continuous(breaks=0:2, labels=c("Ma-1", "Ma-2", "Ma-3"))+ylab(expression("Salicylic acid (ng g"^-1* " FW)")) + 
+  xlab("Macronutrients")+theme_bw() + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),axis.text.x = element_text(size=30,vjust=.7),axis.title.y = element_text(size=25),axis.title.x = element_text(size=25),legend.title = element_text(size=30),axis.text = element_text(size = 30),strip.text.x = element_text(size = 30))
+poltcaMa+geom_smooth(method = "lm", se = FALSE)+coord_cartesian(ylim = c(300,1800))
+
+#Plot polynomial regression of micronutrients sick vs healthy plants
+Plot_SA_micros_poly$Sanidad <- factor(Plot_SA_micros_poly$Sanidad, levels = c("HLB+", "HLB-")) #Sort health status data
+poltcaMi <-  ggplot(data = Plot_SA_micros_poly, aes(x = Factor, y = SA, fill=Sanidad)) + geom_point(aes(shape=Sanidad,stroke = 2),size=3)+geom_smooth(method = "lm", formula = y ~ poly(x,2),color="black")+ scale_fill_manual(values = c("black","gray80")) + scale_x_continuous(breaks=0:2, labels=c("Ma-1", "Ma-2", "Ma-3"))+ylab(expression("Salicylic acid (ng g"^-1* " FW)")) + 
+  xlab("Macronutrients")+theme_bw() + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),axis.text.x = element_text(size=30,vjust=.7),axis.title.y = element_text(size=25),axis.title.x = element_text(size=25),legend.title = element_text(size=30),axis.text = element_text(size = 30),strip.text.x = element_text(size = 30))
+poltcaMi+geom_smooth(method = "lm", se = FALSE)+coord_cartesian(ylim = c(300,1800))
